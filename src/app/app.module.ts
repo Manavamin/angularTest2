01feebd9a69dfef906909eadb0bb85afa51fe693
Manavamin <manavamin7@gmail.com>
2024-04-19 22:08:51 -0400

    Created Angular component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ManavaminComponent } from './manavamin/manavamin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManavaminComponent
   ],
   imports: [
     BrowserModule,
